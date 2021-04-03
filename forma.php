abe97799237725de31ce1df3aab08d99d2af1986
romanserko <roman_serko@online.ua>
2021-04-04 00:48:19 +0300

    видалив index.html

--- forma.php
+++ forma.php
@@ -80,11 +80,11 @@ function send()
      <td colspan="2">
 	 <font size="4"> 
 	 <b>
-	 <a href="#"> Головна </a>&nbsp;&nbsp;
- 	 <a href="#"> Фотогалерея </a>&nbsp;&nbsp;
+	 <a href="index.php"> Головна </a>&nbsp;&nbsp;
+ 	 <a href="fotogalereya.php"> Фотогалерея </a>&nbsp;&nbsp;
  	 <a href="#"> Телефони </a>&nbsp;&nbsp;
- 	 <a href="#"> Статистика </a>&nbsp;&nbsp;
- 	 <a href="#"> Зареєстровані </a>&nbsp;&nbsp;
+ 	 <a href="statystyka.php"> Статистика </a>&nbsp;&nbsp;
+ 	 <a href="zareyestrovany.php"> Зареєстровані </a>&nbsp;&nbsp;
 	 
 	</b>
 	</font>
@@ -145,14 +145,13 @@ function send()
  <!-- ---- текст сайту ---- -->
   <td width="70%" > 
   
-  <h1 align="center"> Дякуємо за реєстрацію 
+  <h1 align="center"> Дякуємо за реєстрацію </h1>	
     <?php  
 	$st=$_post['name2'].";".$st=$_post['name1'].";".$st=$_post['nicl'].";".$st=$_post['email'].";".$st=$_post['password']."\n";
 	$fp = fopen("baza.txt","a");
 	$test = fwrite($fp,$st);
 	echo "<h2 align='center'>Ви ввели :".$_post['name2'].";".$st=$_post['name1'].";".$st=$_post['nicl'].";".$st=$_post['email'].";".$st=$_post['password']."</h2>"
 	?>
-  </h1>	
   
 </td>
 </tr>
